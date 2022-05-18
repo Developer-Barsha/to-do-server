94a7c417c024e7129bd0c1501fb8cb3e942341d1
Developer-Barsha <lamiatabassumbarsha@gmail.com>
2022-05-19 04:44:15 +0600

    update api done

--- index.js
+++ index.js
@@ -12,47 +12,58 @@ app.use(express.json());
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.0uudq.mongodb.net/?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
-async function run(){
+async function run() {
     await client.connect();
     const taskCollection = client.db('todo-app').collection('task');
 
-    try{
-        app.get('/tasks', async (req, res)=>{
+    try {
+        app.get('/tasks', async (req, res) => {
             const tasks = await taskCollection.find().toArray();
             res.send(tasks);
         });
-        
-        app.get('/tasks/:email', async (req, res)=>{
+
+        app.get('/tasks/:email', async (req, res) => {
             const email = req.params.email;
-            const query = {email : email};
+            const query = { email: email };
             const tasks = await taskCollection.find(query).toArray();
             res.send(tasks);
         });
 
-        app.post('/tasks', async(req, res)=>{
+        app.post('/tasks', async (req, res) => {
             const task = req.body;
             const result = await taskCollection.insertOne(task);
             res.send(result);
         })
 
-        app.delete('/tasks/:id', async (req, res)=>{
+        app.put('/tasks/:id', async (req, res) => {
             const id = req.params.id;
-            const query = {_id : ObjectId(id)};
+            const query = { _id: ObjectId(id) };
+            const options = { upsert: true };
+            const updateDoc = {
+                $set: { complete: true }
+            }
+            const result = await taskCollection.updateOne(query, updateDoc, options);
+            res.send(result)
+        })
+
+        app.delete('/tasks/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: ObjectId(id) };
             const result = await taskCollection.deleteOne(query);
             res.send(result)
         })
     }
-    finally{
+    finally {
 
-    }    
+    }
 }
 run().catch(console.dir)
 
-app.get('/', (req, res)=>{
+app.get('/', (req, res) => {
     res.send('Hello from to do server');
 })
 
 
-app.listen(port, ()=>{
+app.listen(port, () => {
     console.log('server is running on port', port);
 })
